70ffc0f5cad7e18aebf9a2e2d45783c6e0e9b962
Mohamed Hilal <118242678+DePrince02@users.noreply.github.com>
2023-03-09 15:36:34 +0530

    Add files via upload

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,222 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "MOHAMED HILAL",
+    post: "WEB DESIGNER",
+    design: "UI / UX DESIGNER",
+    desc: "React.js Developers Design And Implement User Interface Components For JavaScript-Based Web And Mobile Applications Using The React Open-Source Library Ecosystem. These Skilled Front-End Developers Are Involved In All Stages Of Interface Component Design,zFrom Conception Through To Final Testing.",
+  },
+]
+export const about = [
+  {
+    desc: "Hi This Is Mohamed Hilal, I Welcome You All To Explore My PortFolio, I Completed My SSLC and HSC in G.G.S.M.M.HR Secondary School-Mayiladuthurai, And Also Completed My Graduate Computer Science In A.V.C College (Autonous)-Mayilathurai, Securing 8.72 CGPA.  ",
+    desc1: "Then I Want To Upgrade My Skill, So I Joined SLA Institude To Updrage My Skills Then After I Became An WEB DEVELOPER. I'm Very Well Knowledge About HTML,CSS,Javascript And Also React.Js ",
+    cover: "./images/man.png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Creative Design",
+    desc: "Creative Design Is A Process That Uses Digital And Physical Image-Making Tools",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Clean Code",
+    desc: "Clean Code Is Clear, Understandable, And Maintainable. When You Write Clean Code",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Design Approach That Addresses The Range Of Devices And Device Sizes.",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "Open-Source React Component Library That Implements Google's Material Design.",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Material UI Icons",
+    desc: "A Pre-Made Set Of Icons That Can Be Extracted From The MUI Component System.",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "The Most Versatile And Feature-Rich Support Plugin For WordPress.",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "07",
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "27",
+    title: "PROJECTS COMPLETED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "15",
+    title: "FILES DOWNLOADED",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "126",
+    title: "LIENS OF CODE",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/port1.jpg",
+    name: "ANOX",
+    category: "marketing",
+    title: "Wear Helmet",
+  },
+  {
+    id: 2,
+    cover: "../images/port/port2.jpg",
+    name: "APPLE",
+    category: "design",
+    title: "Smart Watch",
+  },
+  {
+    id: 4,
+    cover: "../images/port/port4.jpg",
+    name: "PETIT NATURALS",
+    category: "marketing",
+    title: "Drink Pure",
+  },
+  {
+    id: 5,
+    cover: "../images/port/port5.jpg",
+    name: "Devine Pot",
+    category: "design",
+    title: "KRIFFLIG",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "Success Is No Accident. It Is Hard Work, Perseverance, Learning, Studying, Sacrifice And Most Of All, Love Of What You Are Doing Or Learning To Do.",
+    image: "./images/testimonials/team-1.png",
+    name: "Mohamed Hilal",
+    post: "React Developer",
+  },
+  {
+    id: 2,
+    text: "You Can’t Go Back And Change The Beginning, But You Can Start Where You Are And Change The Ending.",
+    image: "./images/testimonials/team-2.png",
+    name: "Ukesh Shiva",
+    post: "Front End Developer",
+  },
+  {
+    id: 3,
+    text: "Yesterday I Was Clever, So I Wanted To Change The World. Today I Am Wise, So I Am Changing Myself.",
+    image: "./images/testimonials/team-3.png",
+    name: "Preethi",
+    post: "Back End Developer",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Master These Awesome",
+    date: "Dec 31, 2022",
+    author: "Robert Downey",
+    desc: " you learn how to do it properly or you succeed in understanding it completely",
+    cover: "./images/blog/b1.png",
+  },
+  {
+    id: 2,
+    title: "Best Design Items to Appeal",
+    date: "Feb 02, 2022",
+    author: "Steven Bill",
+    desc: "you state the situation or event, explain why you think it was wrong or unjust.",
+    cover: "./images/blog/b2.png",
+  },
+  {
+    id: 3,
+    title: "The 20 Best Lightroom Presets",
+    date: "Jan 05, 2022",
+    author: "Mark Antony",
+    desc: " A Lightroom preset is a filter with predetermined settings.",
+    cover: "./images/blog/b3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "No 2/45 C.N.K Road",
+    text2: "Triplicane,Chennai,600005",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "6383514855",
+    text2: "9597260991",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "mhilal965@gmail.com",
+    text2: "mhilal965@outlook.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
